a98c7a3c1878bb62d7387df6e89d5b3148180272
MarcoSaverino <marcosaverino@live.ca>
2023-08-23 11:45:02 -0400

    made some small changes to App.js to optimize code structure

--- src/App.js
+++ src/App.js
@@ -1,45 +1,41 @@
 import React, { useState } from "react";
-import Header from "../src/components/Header";
-import About from "../src/components/About";
-import Contact from "../src/components/Contact";
-import Portfolio from "../src/components/Portfolio";
-import Resume from "../src/components/Resume";
-import Footer from "../src/components/Footer";
+import Header from "./components/Header";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Portfolio from "./components/Portfolio";
+import Resume from "./components/Resume";
+import Footer from "./components/Footer";
 import { Helmet } from "react-helmet";
 import "./App.css";
 
 function App() {
-  const [currentTab, handleTabChange] = useState("about");
+  const [currentTab, handleTabChange] = useState("About");  // Adjusted to have a consistent case
 
-  // This method is checking to see what the value of `currentTab` is. Depending on the value of currentPage, we return the corresponding component to render.
-  const renderTab = () => {
-    if (currentTab === "About") {
-      return <About />;
-    }
-    if (currentTab === "Contact") {
-      return <Contact />;
-    }
-    if (currentTab === "Portfolio") {
-      return <Portfolio />;
-    }
-    if (currentTab === "Resume") {
-      return <Resume />;
-    }
-    return <About />;
+  // Object map to manage tabs and their corresponding components
+  const tabComponents = {
+    About: <About />,
+    Contact: <Contact />,
+    Portfolio: <Portfolio />,
+    Resume: <Resume />
   };
+
+  // Rendering tab based on the currentTab's value
+  const renderTab = () => tabComponents[currentTab] || <About />;
 
   return (
     <>
       <Helmet>
         <title>Marco Saverino's Portfolio| {currentTab} </title>
       </Helmet>
-      ;
+      
       <Header
         currentTab={currentTab}
         handleTabChange={handleTabChange}
-      ></Header>
+      />
+      
       <main>{renderTab()}</main>
-      <Footer></Footer>
+      
+      <Footer />
     </>
   );
 }
